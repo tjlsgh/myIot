dc32012713ec2ce46892ef18bcf71150cb2e0063
tjlsgh <1428562224@qq.com>
2020-10-18 17:13:38 +0800

    可以回复服务器发送来的命令

--- nodemcu/nodemcu.ino
+++ nodemcu/nodemcu.ino
@@ -11,15 +11,21 @@ const char* password = "hhxxttxs";
 
 const char* deviceId = "mydevice1";
 int tick = 1000;
-
+char data[50] = "";
+char res[50] = "";
 WiFiClient client;
 dht11 DHT11;
+int lightState; // 灯泡状态
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println();
 
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  lightState = 0;
+  
   WiFi.begin(ssid, password);
   Serial.print("Connecting");
   client.setTimeout(100);
@@ -52,25 +58,26 @@ void loop() {
       if (line == "1") {
         Serial.println("command:open led.");
         digitalWrite(LED_BUILTIN, LOW);
-        client.print("OK");
+        sprintf(res, "{\"type\":1,\"device\":{\"light1\": 1}}"); // 开
+        client.print(res);
       }
       else if (line == "0") {
         Serial.println("command:close led.");
         digitalWrite(LED_BUILTIN, HIGH);
-        client.print("OK");
+        sprintf(res, "{\"type\":1,\"device\":{\"light1\": 0}}"); // 关
+        client.print(res);
       }
     } else { // 发送数据
       int chk = DHT11.read(DHT11_PIN);
       Serial.println(chk);
       // 如果没读取到数据则发送随机数
-      if(chk != DHTLIB_OK) {
-        client.print(10 + random(0 + 20));
-        
-      } else {
-        client.print(DHT11.temperature);
+      if(chk != DHTLIB_ERROR_TIMEOUT) {
+        sprintf(data, "{\"type\": 0, \"temp\": %d, \"humi\": %d, \"light1\": %d}", DHT11.temperature, DHT11.humidity, lightState);
+      } else {        
+        sprintf(data, "{\"type\": 0, \"temp\": %d, \"humi\": %d,  \"light1\": %d}", 20 + random(0 + 10), 50 + random(0 + 10), lightState);
       }
-      Serial.print("temp:");
-      Serial.println(DHT11.temperature);
+      client.print(data);
+      Serial.println(data);
       delay(1000);
     }
   }
